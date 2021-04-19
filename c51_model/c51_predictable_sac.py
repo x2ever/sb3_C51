20c15baed4fd468cc23b5dcc10c67b9ab4ac3afe
necro <necrocathy@gmail.com>
2021-04-19 18:25:38 +0900

    Update CMV policy

--- c51_model/c51_predictable_sac.py
+++ c51_model/c51_predictable_sac.py
@@ -1,18 +1,18 @@
 from stable_baselines3.sac.sac import *
-from c51_model.c51 import C51SACPolicy
+from c51_model.c51_predictable_policy import CMVC51SACPolicy
 from stable_baselines3.common.buffers import ReplayBuffer
-from datetime import timedelta
 import time
+from datetime import timedelta
 
 
-class C51SAC(OffPolicyAlgorithm):
+class CMVCVaRSAC(OffPolicyAlgorithm):
     def __init__(
         self,
-        policy: Union[str, Type[C51SACPolicy]],
+        policy: Union[str, Type[CMVC51SACPolicy]],
         env: Union[GymEnv, str],
-        min_v: float,
-        max_v: float,
-        support_dim: int,
+        min_v: float = -25,
+        max_v: float = +25,
+        support_dim: int = 200,
         learning_rate: Union[float, Schedule] = 3e-4,
         buffer_size: int = int(1e6),
         learning_starts: int = 100,
@@ -37,10 +37,10 @@ class C51SAC(OffPolicyAlgorithm):
         device: Union[th.device, str] = "auto",
         _init_setup_model: bool = True,
     ):
-        super(C51SAC, self).__init__(
+        super(CMVCVaRSAC, self).__init__(
             policy,
             env,
-            C51SACPolicy,
+            CMVC51SACPolicy,
             learning_rate,
             buffer_size,
             learning_starts,
@@ -77,8 +77,7 @@ class C51SAC(OffPolicyAlgorithm):
         self.supports = th.from_numpy(
             np.array([min_v + i * self.interval for i in range(support_dim)], dtype=np.float32)
         ).to(self.device)
-        self.total_timesteps = None
-
+        self._total_timesteps = None
         if _init_setup_model:
             self._setup_model()
 
@@ -137,6 +136,9 @@ class C51SAC(OffPolicyAlgorithm):
         self.actor = self.policy.actor
         self.critic = self.policy.critic
         self.critic_target = self.policy.critic_target
+        self.cmv_net = self.policy.cmv_net
+        self.beta_critic = self.policy.beta_critic
+        self.beta_critic_target = self.policy.beta_critic_target
 
     def projection(self, support_rows, target_z):
         projected_target_z = th.zeros_like(target_z)
@@ -158,8 +160,10 @@ class C51SAC(OffPolicyAlgorithm):
         self._update_learning_rate(optimizers)
 
         ent_coef_losses, ent_coefs = [], []
-        actor_losses, critic_losses = [], []
+        actor_losses, critic_losses, reward_losses, feature_pred_losses = [], [], [], []
+        cvars = []
         qs = []
+        critic_beta_losses = []
 
         for gradient_step in range(gradient_steps):
             # Sample replay buffer
@@ -204,13 +208,14 @@ class C51SAC(OffPolicyAlgorithm):
                 # td error + entropy term
                 target_supports = replay_data.rewards + (1 - replay_data.dones) * self.gamma * target_supports
 
+
             # Get current Q-values estimates for each critic network
             # using action from the replay buffer
             current_zs = th.cat(self.critic(replay_data.observations, replay_data.actions), dim=1)
             target_zs = self.projection(target_supports, target_zs)
             # Compute critic loss
 
-            critic_loss = -th.mean(th.log(current_zs) * target_zs)
+            critic_loss = -th.mean(th.log(current_zs + 1e-12) * target_zs)
             critic_losses.append(critic_loss.item())
 
             # Optimize the critic
@@ -218,13 +223,68 @@ class C51SAC(OffPolicyAlgorithm):
             critic_loss.backward()
             self.critic.optimizer.step()
 
+            # Compute
+            # For the CMV Learning
+            with th.no_grad():
+                next_z = self.critic_target.features_extractor(replay_data.next_observations)
+            # predicted reward, and predicted next observation
+            r_pred, z_pred = self.cmv_net(replay_data.observations, replay_data.actions)
+            mse_r_pred = th.mean(th.square(r_pred - replay_data.rewards))
+            mse_z_pred = th.mean(th.square(z_pred - next_z))
+            loss_cmv = mse_r_pred + mse_z_pred
+
+            reward_losses.append(mse_r_pred.item())
+            feature_pred_losses.append(mse_z_pred.item())
+
+            # Optimize the CMV Nets
+            self.cmv_net.optimizer.zero_grad()
+            loss_cmv.backward()
+            self.cmv_net.optimizer.step()
+
+            with th.no_grad():
+                # Select action according to policy
+                # Compute the next Q values: min over all critics targets
+                next_q_beta_values = th.cat(self.beta_critic_target(replay_data.next_observations, next_actions), dim=1)
+                next_q_beta_values, _ = th.min(next_q_beta_values, dim=1, keepdim=True)
+                # add entropy term
+                next_q_beta_values = next_q_beta_values - ent_coef * next_log_prob.reshape(-1, 1)
+                # td error + entropy term
+                target_q_beta_values = loss_cmv.detach() + (1 - replay_data.dones) * (
+                            self.gamma ** 2) * next_q_beta_values
+
+            # Get current Q-beta values estimates for each critic network
+            # using action from the replay buffer
+            current_q_beta_values = self.beta_critic(replay_data.observations, replay_data.actions)
+
+            # Compute critic beta loss
+            critic_beta_loss = \
+                0.5 * sum(
+                    [F.mse_loss(current_q_beta, target_q_beta_values) for current_q_beta in current_q_beta_values])
+            critic_beta_losses.append(critic_beta_loss.item())
+
+            # Optimize the critic beta
+            self.beta_critic.optimizer.zero_grad()
+            critic_beta_loss.backward()
+            self.beta_critic.optimizer.step()
+
             # Compute actor loss
-            # Alternative: actor_loss = th.mean(log_prob - qf1_pi)
+            # Alternative: actor_loss = th.mean(log_prob - qf1_pi - qf1_beta_pi)
             # Mean over all critic networks
             z_pi = th.cat(self.critic.forward(replay_data.observations, actions_pi), dim=1)
-            q_pi = z_pi @ self.supports
-            qs.append(th.mean(q_pi).item())
-            actor_loss = (ent_coef * log_prob - q_pi).mean()
+
+            z_cdf = th.cumsum(z_pi, dim=-1)
+            adjust_pdf = th.where(
+                th.le(z_cdf, 0.3),
+                z_pi,
+                th.zeros_like(z_pi)
+            )
+            adjust_pdf = th.div(adjust_pdf, th.sum(adjust_pdf, dim=-1, keepdim=True))
+            q_pi = adjust_pdf @ self.supports
+            cvars.append(th.mean(q_pi).item())
+            qs.append(th.mean(z_pi @ self.supports).item())
+            q_beta_values_pi = th.cat(self.beta_critic.forward(replay_data.observations, actions_pi), dim=1)
+            max_qf_beta_pi, _ = th.max(q_beta_values_pi, dim=1, keepdim=True)
+            actor_loss = (ent_coef * log_prob - q_pi + next_q_beta_values).mean()
             actor_losses.append(actor_loss.item())
 
             # Optimize the actor
@@ -235,19 +295,23 @@ class C51SAC(OffPolicyAlgorithm):
             # Update target networks
             if gradient_step % self.target_update_interval == 0:
                 polyak_update(self.critic.parameters(), self.critic_target.parameters(), self.tau)
+                polyak_update(self.beta_critic.parameters(), self.beta_critic_target.parameters(), self.tau)
 
         self._n_updates += gradient_steps
-
         fps = int(self.num_timesteps / (time.time() - self.start_time))
-        remaining_steps = self.total_timesteps - self.num_timesteps
+        remaining_steps = self._total_timesteps - self.num_timesteps
 
         eta = int(round(remaining_steps / fps))
         logger.record("time/eta", timedelta(seconds=eta), exclude="tensorboard")
-        logger.record("train/Q", np.mean(qs))
+        logger.record("train/CVaR", np.mean(cvars))
+        logger.record("train/Q-value", np.mean(qs))
         logger.record("train/n_updates", self._n_updates, exclude="tensorboard")
         logger.record("train/ent_coef", np.mean(ent_coefs))
         logger.record("train/actor_loss", np.mean(actor_losses))
         logger.record("train/critic_loss", np.mean(critic_losses))
+        logger.record("train/reward error", np.mean(reward_losses))
+        logger.record("train/s_t+1_error", np.mean(feature_pred_losses))
+        logger.record("train/beta_Q_loss", np.mean(critic_beta_losses))
         if len(ent_coef_losses) > 0:
             logger.record("train/ent_coef_loss", np.mean(ent_coef_losses))
 
@@ -263,8 +327,8 @@ class C51SAC(OffPolicyAlgorithm):
         eval_log_path: Optional[str] = None,
         reset_num_timesteps: bool = True,
     ) -> OffPolicyAlgorithm:
-        self.total_timesteps = total_timesteps
-        return super(C51SAC, self).learn(
+
+        return super(CMVCVaRSAC, self).learn(
             total_timesteps=total_timesteps,
             callback=callback,
             log_interval=log_interval,
@@ -277,7 +341,7 @@ class C51SAC(OffPolicyAlgorithm):
         )
 
     def _excluded_save_params(self) -> List[str]:
-        return super(C51SAC, self)._excluded_save_params() + ["actor", "critic", "critic_target"]
+        return super(CMVCVaRSAC, self)._excluded_save_params() + ["actor", "critic", "critic_target"]
 
     def _get_torch_save_params(self) -> Tuple[List[str], List[str]]:
         state_dicts = ["policy", "actor.optimizer", "critic.optimizer"]
@@ -296,6 +360,52 @@ if __name__ == "__main__":
     env = NavigationEnvAcc({"OBSTACLE_POSITIONS": obs_set[1], "Goal": goal_set[-1]})
 
     # model = SAC(SACPolicy, env, verbose=1)
-    model = C51SAC(C51SACPolicy, env, min_v=-20, max_v=20, support_dim=64, verbose=1)
-    model.learn(100000)
+    model = CMVCVaRSAC(CMVC51SACPolicy, env, min_v=-25, max_v=25, support_dim=200, verbose=1)
+    model.learn(300000)
+    # model.save("CVaR-0.3")
+
+
+    def evaluate(env, model, ep_n, alpha):
+        logs = []
+        for _ in range(ep_n):
+            obs = env.reset()
+            ep_reward = 0
+            ep_log = []
+            while True:
+                action, _ = model.predict(obs, deterministic=True)
+                next_obs, reward, done, info = env.step(action)
+                z_pi = th.cat(model.critic.forward(th.from_numpy(np.array([obs])).cuda(),
+                                                   th.from_numpy(np.array([action])).cuda()), dim=1)
+                z_cdf = th.cumsum(z_pi, dim=-1)
+                adjust_pdf = th.where(
+                    th.le(z_cdf, alpha),
+                    z_pi,
+                    th.zeros_like(z_pi)
+                )
+                adjust_pdf = th.div(adjust_pdf, th.sum(adjust_pdf, dim=-1, keepdim=True))
+                cvar = adjust_pdf @ model.supports
+                log = [obs, next_obs, reward, done, info,
+                       z_pi.cpu().detach().numpy(), cvar.cpu().detach().numpy()]
+                ep_log.append(log)
+                obs = next_obs
+                ep_reward += reward
+                if done:
+                    logs.append(ep_log)
+                    break
+        return logs
+
+
+    import pickle
+    for alpha in [0.2, 0.3, 0.4, 1.0]:
+        model = CMVCVaRSAC.load(f"CVaR-{alpha}", min_v=-25, max_v=25, support_dim=200)
+        log = evaluate(env, model, 1000, alpha)
+        data = {
+            "min_v": 25,
+            "max_v": 25,
+            "support_dim": 200,
+            "alpha": alpha,
+            "data": log
+        }
+        with open(f'alpha{alpha}_ep_log.txt', 'wb') as f:
+            pickle.dump(data, f)
 
